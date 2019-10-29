2432847ed790f2bca4ad0191511bac2acea1cd2b
IINamelessII <justnitro26@gmail.com>
2019-10-29 19:00:53 +0200

    Write sources('/src'), CMakeLists.txt-s, run 'cmake'  and 'cmake -G ninja'

---
+++ CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nitro/Study_Local/AK_linux/lab4/src/main.cpp" "/home/nitro/Study_Local/AK_linux/lab4/CMakeFiles/main.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "src/hello"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/nitro/Study_Local/AK_linux/lab4/hello/CMakeFiles/hello.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
